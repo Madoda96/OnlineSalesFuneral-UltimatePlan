70a2a6b825404b8ff7f7d4c72afd846bc2b0eb72
MaMazibuko <MaMazibuko@clientele.co.za>
2023-06-08 12:10:06 +0200

    added Web elements and methods  for additionals members but only the spouse details the remaining webelements will be added as we add more test cases. Also implemented the steps definition for GIVEN and When.

--- src/test/java/PageObjects/AdditionalMembers.java
+++ src/test/java/PageObjects/AdditionalMembers.java
@@ -1,15 +1,139 @@
 package PageObjects;
 
+import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.Step;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
-public class AdditionalMembers {
+import java.util.List;
+
+public class AdditionalMembers extends PageObject {
 
 
-  @Step("Add a spouse ")
-  public void addSpouse(){
+  // Add spouse Webelements
+  String AddSpouseCheckbxXpath = "//*[@id=\"onlineSaleForm\"]/div/div[2]/div[1]/div[7]/fieldset/div[1]/div[1]/label/span";
 
+  String TitleXpath = "//select[@id=\"onTtlSp\"]";
+
+  String Spouse_NameXpath = "//input[@name=\"onNameSp\"]";
+
+  String Spouse_SurnameNameXpath = "//input[@name=\"onSurnameSp\"]";
+
+  String Spouse_GenderXpath = "//select[@id=\"onGenderSp\"]";
+
+  String Spouse_IDXpath = "//input[@name=\"onIdNumSp\"]";
+
+  String Spouse_DoBXpath = "//input[@name=\"onDob\"]";
+  String CalenderYear = "//select[@class=\"ui-datepicker-year\"]";
+
+  String CalenderMonth = "//select[@class=\"ui-datepicker-month\"]";
+
+  String SaveBtnXpath = "//div[@id=\"SaveSpouse\"]";
+
+  String DeleteBtnXpath = "//div[@class=\"butDelete\"]";
+
+  String EditBtnXpath = "//div[@class=\"butEdit\"]";
+
+  String ContinueBtnXpath = "//div[@onclick=\"loader();step('2','no');\"]";
+
+  String BackBtnXpath = "//div[@onclick=\"stepback('0');\"]";
+
+
+  @Step("Check checkbox to add a spouse ")
+  public void addSpouse() {
+    $(By.xpath(AddSpouseCheckbxXpath)).click();
 
   }
+
+  @Step("select spouse Title")
+  public void selectTitle(String title) {
+    $(By.xpath(TitleXpath)).sendKeys(title);
+  }
+
+  @Step("Enter spouse name")
+  public void enterSpouseName(String spouse_Name) {
+    $(By.xpath(Spouse_NameXpath)).sendKeys(spouse_Name);
+
+  }
+
+  @Step("Enter spouse surname")
+  public void enterSpouseSurname(String surname) {
+    $(By.xpath(Spouse_SurnameNameXpath)).sendKeys(surname);
+
+  }
+
+  @Step("Select spouse gender")
+  public void selectGender(String Gender) {
+    Select gender = $(By.xpath(Spouse_GenderXpath));
+    gender.selectByValue(Gender);
+
+  }
+
+  @Step(" Enter spouse ID number")
+  public void EnterSpouseIDNo(String IdNum) {
+    $(By.xpath(Spouse_IDXpath)).sendKeys(IdNum);
+  }
+
+  @Step("enter spouse date of birth")
+  public void enterDateOfBirth(String Year, String Month, String day) {
+
+    String activedatesXpath = "//*[@id=\"ui-datepicker-div\"]/table";
+
+    $(By.xpath(Spouse_DoBXpath)).click();
+    Select yr = $(By.xpath(CalenderYear));
+    Select mnth = $(By.xpath(CalenderMonth));
+    //  enter the year eg 1990,1991,1992. etc
+    yr.selectByVisibleText(Year);
+
+    // enter the month eg Jan,Feb,Mar,April,etc
+    mnth.selectByVisibleText(Month);
+
+    List<WebElement> activedates = $(By.xpath(activedatesXpath));
+
+    for (WebElement dates : activedates) {
+      String date = dates.getText();
+//      System.out.println(date);
+      if (date.equals(day)) {
+        dates.click();
+        break;
+      }
+    }
+
+  }
+
+  @Step("Save spouse details ")
+  public void saveSpousedtls(){
+    $(By.xpath(SaveBtnXpath)).click();
+
+  }
+
+@Step("Delete created spouse details")
+public void DeleteSpouse(){
+  Assert.assertTrue($(By.xpath(DeleteBtnXpath)).isDisplayed());
+  $(By.xpath(DeleteBtnXpath)).click();
+
+}
+
+@Step(" Edit spouse details ")
+ public void EditSpouseDetails(){
+    $(By.xpath(EditBtnXpath)).click();
+
+
+}
+@Step("click on Continue to proceed to beneficiaries Details page")
+public void clickContinue(){
+    Assert.assertTrue($(By.xpath(ContinueBtnXpath)).isClickable());
+    $(By.xpath(ContinueBtnXpath)).click();
+
+}
+@Step("Click back button to see member details page")
+public void clickBackBtn(){
+    Assert.assertTrue($(By.xpath(ContinueBtnXpath)).isClickable());
+    $(By.xpath(BackBtnXpath)).click();
+
+}
 
 
 }
--- src/test/java/PageObjects/HomePage.java
+++ src/test/java/PageObjects/HomePage.java
@@ -8,53 +8,60 @@ import org.openqa.selenium.WebElement;
 public class HomePage extends PageObject {
 
     //Funeral Plan WebElements
-    WebElement FuneralBuyNow = $(By.xpath("\"//a[@class=\"btn funeral\"]"));
-    WebElement GetQuoteFuneral = $(By.xpath("//a[@class=\"btn funeral call-me-quote cboxElement\"]"));
+    String FuneralBuyNowXpath = "\"//a[@class=\"btn funeral\"]";
+    String GetQuoteFuneralXpath = "//a[@class=\"btn funeral call-me-quote cboxElement\"]";
 
     // Legal Plan WebElements
 
-    WebElement LegalBuyNow = $(By.xpath("//a[@class=\"btn legal\"]"));
-    WebElement LegalGetQuote = $(By.xpath("//a[@class=\"btn legal call-me-quote cboxElement\"]"));
+    String LegalBuyNowXpath = "//a[@class=\"btn legal\"]";
+    String LegalGetQuoteXpath = "//a[@class=\"btn legal call-me-quote cboxElement\"]";
 
     // Hospital Plans WebElements
-    WebElement HospitalPlanBuyNow = $(By.xpath("//a[@class=\"btn hospital\"]"));
-    WebElement HospitalPlanGetQuote = $(By.xpath("//a[@class=\"btn hospital call-me-quote cboxElement\"]"));
+    String HospitalPlanBuyNowXpath = "//a[@class=\"btn hospital\"]";
+    String HospitalPlanGetQuoteXpath = "//a[@class=\"btn hospital call-me-quote cboxElement\"]";
+
+    String ContinueBtnXpath = "//div[@id=\"onlinego\"]";
+
 
 
     @Step("Click Buy Now on Funeral Plan")
     public void ClickBuyNowFuneralPlans(){
 
-        FuneralBuyNow.click();
+        $(By.xpath(FuneralBuyNowXpath)).click();
 
 
     }
 
+    @Step("Click the continue button")
+    public void clickContinueBtn(){
+        $(By.xpath(ContinueBtnXpath)).click();
 
+    }
     @Step("Click Get a Quote on Funeral Plan")
     public void GetAQuoteFuneralPlans(){
-    GetQuoteFuneral.click();
+        $(By.xpath(GetQuoteFuneralXpath)).click();
 
     }
     @Step("Click Buy Now on legal Plan")
     public void ClickBuyNowLegalPlans(){
-        LegalBuyNow.click();
+        $(By.xpath(LegalBuyNowXpath)).click();
 
     }
 
     @Step("Click Get Quote on legal Plan")
     public void ClickGetQuoteLegalPlans(){
-        LegalGetQuote.click();
+        $(By.xpath(LegalGetQuoteXpath)).click();
 
     }
 
     @Step("Click Buy Now on Hospital Plan")
     public void ClickBuyNowHospitalPlans(){
-    HospitalPlanBuyNow.click();
+    $(By.xpath(HospitalPlanBuyNowXpath)).click();
 
     }
     @Step("Click Get quote on Hospital Plan")
     public void ClickGetAQuoteFuneralPlans(){
-        HospitalPlanGetQuote.click();
+        $(By.xpath(HospitalPlanGetQuoteXpath)).click();
 
     }
 
