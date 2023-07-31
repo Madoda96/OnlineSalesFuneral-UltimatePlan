f06b0653d203ffb2e0764db22a59c9ada8d79fde
Madoda96 <madodaash@gmail.com>
2023-07-31 10:30:25 +0200

    updated test cases

--- src/test/java/runner/TesterUltimateDignityPlan.java
+++ src/test/java/runner/TesterUltimateDignityPlan.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(plugin = {"pretty","html:test-output"},
         features = "src/test/resources/features/Ultimate Dignity Plan",
         glue = "stepsDefinition",
-        monochrome = true,
-        dryRun = false  )
+        monochrome = true
+)
  //        tags = "@Regressionz"  )
 public class TesterUltimateDignityPlan {
 
