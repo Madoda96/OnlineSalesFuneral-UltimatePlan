9465d16ae97dd81d911bee8a2d2672c4ebebce31
sarveshpadayachy <sarvesh.padayachy@gmail.com>
2023-11-02 13:59:41 +0200

    Add test cases to Standard HELP

---
+++ src/test/java/runner/H/E/L/P/TesthelpforStandard.java
@@ -0,0 +1,20 @@
+package runner.H.E.L.P;
+
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(plugin = {"pretty", "json:target/CucumberTestReport.json"},
+        features = "src/test/resources/features/HELP/Clientele_Standard_H.E.L.P_Plan.feature",
+        glue = "stepsDefinition",
+        monochrome = true,
+        dryRun = false
+
+
+
+)
+
+public class TesthelpforStandard {
+}
